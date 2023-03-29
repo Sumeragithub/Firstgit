a25547b0e3a9ec6314e461363b9e99606bff1c0b
Sumeragithub <129095793+Sumeragithub@users.noreply.github.com>
2023-03-29 07:06:22 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+class DataTypes
+{
+	public static void main(String args[])
+	{     
+            byte value=4;
+		short s=10000;
+            int sem=8;
+            long mobileNo=123456;
+            float marks=90.7f;
+            double salary=876542.99;
+		char grade='O';
+		boolean flag=true;
+
+		System.out.println("Byte value:" +value);
+		System.out.println("Short value:" +s);
+		System.out.println("Integer value:" +sem);
+		System.out.println("Long value:" +mobileNo);
+		System.out.println("Float value:" +marks);
+		System.out.println("Double value:" +salary);
+		System.out.println("Character value:" +grade);
+		System.out.println("Boolean value:" +flag);
+         }
+	}
