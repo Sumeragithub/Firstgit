ff7d8f3276c179c94c06ee282baed4d8630b3230
Sumeragithub <129095793+Sumeragithub@users.noreply.github.com>
2023-03-29 07:12:18 +0530

    Add files via upload

---
+++ FriendsName.java
@@ -0,0 +1,16 @@
+public class FriendsName
+{
+	private static void displayFriends()
+	{
+		System.out.println("Sushma");
+		System.out.println("Swarna");
+		System.out.println("Subashini");
+		System.out.println("Snega");
+		System.out.println("Gomathi");
+	}
+	public static void main(String args[])
+	{
+		System.out.println("My friends :");
+		displayFriends();
+	}
+}
