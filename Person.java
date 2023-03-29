82a89a1778754748b2823694fe59803fec52b1ef
Sumeragithub <129095793+Sumeragithub@users.noreply.github.com>
2023-03-29 07:15:35 +0530

    added

---
+++ Person.java
@@ -0,0 +1,14 @@
+class Employee
+{
+public static void main(String args[])
+{
+System.out.println("Hello");
+}
+}
+public class Person
+{
+public static void main(String[]args)
+{
+System.out.println("Welcome");
+}
+}
