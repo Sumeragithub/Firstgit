5d73064a32b29a5cd19dbf3cd42fa23490d5a7df
Sumeragithub <129095793+Sumeragithub@users.noreply.github.com>
2023-03-29 07:13:35 +0530

    Add files via upload

---
+++ MyFamily.java
@@ -0,0 +1,15 @@
+public class MyFamily
+{
+	public static void main(String args[])
+	{
+		String mother="Nafees Bhanu",father="Abdul Kalam",phone_number="9876543210",city="Kallakurichi",state="TamilNadu",country="India";
+		int age=23,salary=10000;
+            System.out.println("Mother Name:"+mother);
+		System.out.println("Father Name:"+father);
+		System.out.println("Age:"+age);
+		System.out.println("Salary:"+salary);
+            System.out.println("PhoneNumber:"+phone_number);
+		System.out.println("Address:" +city+ " " +state+" " +country);
+	}
+}
+
